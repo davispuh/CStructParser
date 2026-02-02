bb291a58617ad02c9d8cea4c083fc4e0dcf33860
Dāvis Mosāns <davispuh@gmail.com>
2026-02-02 22:58:22 +0200

    Use better example for README and test

--- README.md
+++ README.md
@@ -21,22 +21,53 @@ gem install CStructParser
 ### For usage with [FFI](https://github.com/ffi/ffi)
 ```ruby
 require 'cstruct_parser'
-sourceCode = 'struct s { int a; float b; };' # Can be path to file aswell
+
+sourceCode = %{
+  #include <stdint.h>
+
+  #define EI_NIDENT (16)
+
+  typedef uint64_t Elf64_Addr;
+  typedef uint16_t Elf64_Half;
+  typedef uint32_t Elf64_Word;
+
+  typedef struct
+  {
+    unsigned char e_ident[EI_NIDENT];
+    Elf64_Half e_type;
+    Elf64_Half e_machine;
+    Elf64_Word e_version;
+    Elf64_Addr e_entry;
+  } Elf64_Ehdr;
+}
+
+# Note that you can specify path to C header file instead of passing string
 CStructParser.to_ffi(sourceCode)
-# class S < FFI::Struct
-#   layout :a, :int,
-#          :b, :float
+# class Elf64Ehdr < FFI::Struct
+#   layout :e_ident, [:uchar, 16],
+#          :e_type, :ushort,
+#          :e_machine, :ushort,
+#          :e_version, :uint,
+#          :e_entry, :ulong
 # end
 ```
 
 ### For usage with [BinData](https://github.com/dmendel/bindata)
 ```ruby
-CStructParser.to_bindata('stuff.h')
-# class S < BinData::Record
+records = CStructParser.to_bindata('stuff.h')
+# class Elf64Ehdr < BinData::Record
 #  endian :little
-#  int32 :a
-#  float :b
+#  array :e_ident, type: :uint8, initial_length: 16
+#  uint16 :e_type
+#  uint16 :e_machine
+#  uint32 :e_version
+#  uint64 :e_entry
 #end
+require 'bindata'
+# Currently we get it as string so need to require/eval but it would be better to implement that objects are directly created
+eval(records)
+Elf64Ehdr.read(File.open('/bin/ruby'))
+# => {e_ident: [127, 69, 76, 70, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0], e_type: 3, e_machine: 62, e_version: 1, e_entry: 4272}
 ```
 
 ### Other use cases
--- spec/CStructParser_spec.rb
+++ spec/CStructParser_spec.rb
@@ -5,23 +5,48 @@ RSpec.describe CStructParser do
         expect(CStructParser::VERSION).not_to be nil
     end
 
-    let (:simpleStruct) { 'struct simple { int a; float b; };' }
+    let (:simpleStruct) {
+        %{
+            #include <stdint.h>
+
+            #define EI_NIDENT (16)
+
+            typedef uint64_t Elf64_Addr;
+            typedef uint16_t Elf64_Half;
+            typedef uint32_t Elf64_Word;
+
+            typedef struct
+            {
+              unsigned char e_ident[EI_NIDENT];
+              Elf64_Half e_type;
+              Elf64_Half e_machine;
+              Elf64_Word e_version;
+              Elf64_Addr e_entry;
+            } Elf64_Ehdr;
+        }
+    }
 
     describe '#to_ffi' do
         it 'can parse simple C struct for FFI' do
             ffi = CStructParser.to_ffi(simpleStruct)
-            expect(ffi).to include('class Simple < FFI::Struct')
-            expect(ffi).to include('layout :a, :int')
-            expect(ffi).to include(':b, :float')
+            expect(ffi).to include('class Elf64Ehdr < FFI::Struct')
+            expect(ffi).to include('layout :e_ident, [:uchar, 16]')
+            expect(ffi).to include(':e_type, :ushort')
+            expect(ffi).to include(':e_machine, :ushort')
+            expect(ffi).to include(':e_version, :uint')
+            expect(ffi).to include(':e_entry, :ulong')
         end
     end
 
     describe '#to_bindata' do
         it 'can parse simple C struct for BinData' do
             ffi = CStructParser.to_bindata(simpleStruct)
-            expect(ffi).to include('class Simple < BinData::Record')
-            expect(ffi).to include('int32 :a')
-            expect(ffi).to include('float :b')
+            expect(ffi).to include('class Elf64Ehdr < BinData::Record')
+            expect(ffi).to include('array :e_ident, type: :uint8, initial_length: 16')
+            expect(ffi).to include('uint16 :e_type')
+            expect(ffi).to include('uint16 :e_machine')
+            expect(ffi).to include('uint32 :e_version')
+            expect(ffi).to include('uint64 :e_entry')
         end
     end
 
